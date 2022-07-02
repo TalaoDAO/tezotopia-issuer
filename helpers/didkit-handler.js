a8f9adf07a2bdc3219ad44ff7fa458b68a10cd54
zvjezdan <zvjezdan>
2022-07-02 18:23:39 +0200

    update key string for the did

--- helpers/didkit-handler.js
+++ helpers/didkit-handler.js
@@ -23,7 +23,7 @@ exports.getVersion = async function () {
 
 exports.getVerificationMethod = async function(privateKey) {
     try {
-        const verificationMethod = await DIDKit.keyToVerificationMethod("tz", privateKey);
+        const verificationMethod = await DIDKit.keyToVerificationMethod("ethr", privateKey);
         return verificationMethod;
     } catch (error) {
         console.log(error)
