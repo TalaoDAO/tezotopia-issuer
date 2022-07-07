c4cffa9d15426f07fcb50611fe714fc14359acbc
zvjezdan <zvjezdan>
2022-07-07 15:58:11 +0200

    updated verification method

--- service/voucher.service.js
+++ service/voucher.service.js
@@ -9,7 +9,7 @@ const updateCredential = async (id, voucher, subjectId, correlation) => {
   const data = {
     blockchainTezos: correlation,
     expirationDate: now.add(voucher.credentialSubject.offers[0].duration, 'days').toDate(),
-    issuanceDate: now.toDate(),
+    issuanceDate: moment().toDate(),
     voucherId: `urn:uuid:${uuid.v4()}`,
     subjectId: subjectId,
   };
